32d9667cd71a7ee5089150df96fce69982d90eb3
Jake Holdom <jakeholdom@hotmail.com>
2019-08-31 16:24:31 +0100

    Project initialisation
    Initialisation of the xcode project

---
+++ EmojiFace/EmojiFace/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EmojiFace
+//
+//  Created by Jake Holdom on 31/08/2019.
+//  Copyright © 2019 Jake Holdom. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
